7ac649749e87ebd591bd5d941cf61aa24c3a6061
Pratik <117148481+Pratiksonkusare@users.noreply.github.com>
2026-06-24 08:37:46 +0530

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -2,9 +2,4 @@ a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 
 print("Addition:", a + b)
-
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-
-print("Addition:", a + b)
 print("Multiplication:", a * b)
