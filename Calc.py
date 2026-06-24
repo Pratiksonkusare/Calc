e88c01b2b5c16ea2502e7d5dd4ddadc32f1d3279
Pratik <117148481+Pratiksonkusare@users.noreply.github.com>
2026-06-24 08:35:48 +0530

    Update Calc.py
    added multiplication in Calc.py

--- Calc.py
+++ Calc.py
@@ -2,3 +2,9 @@ a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 
 print("Addition:", a + b)
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition:", a + b)
+print("Multiplication:", a * b)
